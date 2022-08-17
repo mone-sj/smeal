e9672b62c67494633b781006d776a0b6e20d374c
SJ Kim <mone-sj21@gmail.com>
2022-08-17 15:18:18 +0900

    first setting

---
+++ src/main/java/com/campfire/smeal/SmealApplication.java
@@ -0,0 +1,13 @@
+package com.campfire.smeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campfire/smeal/SmealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campfire.smeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

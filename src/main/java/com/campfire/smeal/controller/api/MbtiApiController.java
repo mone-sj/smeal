94af73ad66d3baf74aab1e6f3cc333d5aebe536b
mongsik2 <104689908+mongsik2@users.noreply.github.com>
2022-09-05 19:15:45 +0900

    update

---
+++ src/main/java/com/campfire/smeal/controller/api/MbtiApiController.java
@@ -0,0 +1,18 @@
+package com.campfire.smeal.controller.api;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class MbtiApiController {
+
+    @PostMapping("/api/mbti")
+    public void submit(){
+        System.out.println("왔음");
+    }
+
+}
